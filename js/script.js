79c6ea8015a1ec10364b3de95b815343f09be59b
Igor <gnezdilov.i.r@gmail.com>
2017-12-01 15:21:35 +0200

    remastered v1.0
    Полная переделка на флексы.

--- js/script.js
+++ js/script.js
@@ -1,65 +1,81 @@
-function initMap() {
-  var mapDiv = document.getElementById('contacts-map');
-  var myLatLng = {lat: 59.938810, lng: 30.323400};
-  var map = new google.maps.Map(mapDiv, {
-    center: myLatLng,
-    zoom: 15
-  });
-  var image = "img/icon-marker.png";
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: image
-  });
-}
-var button_contacts_show = document.querySelector(".contacts-info .btn-contacts");
-var modal_content = document.querySelector(".feedback-wrapper");
-var button_close = document.querySelector(".feedback .feedback-btn-close");
-var button_submit = document.querySelector(".feedback .btn-submit");
-var feedback_form = document.querySelector(".feedback");
-var user_name, user_email;
-if (feedback_form != null) {
-  user_name = feedback_form.querySelector("[type=text]");
-  user_email = feedback_form.querySelector("[type=email]");
-}
-if (button_contacts_show != null) {
-  button_contacts_show.addEventListener("click", function(event) {
-    event.preventDefault();
-    modal_content.classList.add("feedback-wrapper-show");
-  });
-}
-if (button_close != null) {
-  button_close.addEventListener("click", function(event) {
-    event.preventDefault();
-    modal_content.classList.remove("feedback-wrapper-show");
-    feedback_form.classList.remove("modal-error");
-  });
-}
-if (button_submit != null) {
-  button_submit.addEventListener("click", function(event) {
-    event.preventDefault();
-    modal_content.classList.remove("feedback-wrapper-show");
-    feedback_form.classList.remove("modal-error");
-  });
-}
-if (feedback_form != null) {
-  feedback_form.addEventListener("submit", function(event) {
-    if (!user_name.value || !user_email.value) {
-      event.preventDefault();
-      feedback_form.classList.remove("modal-error");
-      feedback_form.offsetWidth = feedback_form.offsetWidth;
-      feedback_form.classList.add("modal-error");
-      console.log("Нужно ввести логин и пароль");
-    }
-  });
-}
-if (modal_content != null) {
-  window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-      if (modal_content.classList.contains("feedback-wrapper-show")) {
-        modal_content.classList.remove("feedback-wrapper-show");
-        feedback_form.classList.remove("modal-error");
-      }
-    }
-  });
+var link = document.querySelector(".address-btn");
+var feedbackPopup = document.querySelector(".modal-feedback");
+var modalClose = document.querySelector(".modal-close");
+var form = feedbackPopup.querySelector("form");
+var login = feedbackPopup.querySelector("[name=name]");
+var mail = feedbackPopup.querySelector("[name=mail]");
+var text = feedbackPopup.querySelector("textarea");
+var blackout = document.querySelector(".wrap-blackout");
+var storageName = localStorage.getItem("name");
+var storageMail = localStorage.getItem("mail");
+
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	feedbackPopup.classList.add("modal-show");
+	blackout.classList.add("blackout");
+
+	if (storageName) {
+		login.value = storageName;
+		mail.focus();
+		if (storageMail) {
+			mail.value = storageMail;
+			text.focus();
+		} else {
+			mail.focus();
+		}
+	} else {
+		login.focus();
+	}
+});
+
+modalClose.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	feedbackPopup.classList.remove("modal-show");	
+	feedbackPopup.classList.remove("modal-error");
+	blackout.classList.remove("blackout");
+})
+
+form.addEventListener("submit", function (evt) {
+	if (!login.value || !mail.value) {
+		evt.preventDefault();
+		feedbackPopup.classList.remove("modal-error");
+		feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+		feedbackPopup.classList.add("modal-error");
+	}   else {
+		localStorage.setItem("name", login.value);
+		localStorage.setItem("mail", mail.value);
+	}
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (feedbackPopup.classList.contains("modal-show")) {
+			feedbackPopup.classList.remove("modal-show");
+			feedbackPopup.classList.remove("modal-error");
+			blackout.classList.remove("blackout");
+		}
+	}
+});
+
+// Карта
+
+ymaps.ready(init);
+
+function init() {
+
+	var myMap;
+
+	myMap = new ymaps.Map("map", {
+		center: [59.93937397, 30.32929868],
+		zoom: 16,
+		controls: []
+	});
+
+	var myPlacemark = new ymaps.Placemark([59.93873873, 30.32299013] , {}, {
+		iconLayout: "default#image",
+		iconImageHref: "img/pin.png",
+		iconImageSize: [218, 142],
+		iconImageOffset: [-40, -131] });     
+
+	myMap.geoObjects.add(myPlacemark);
 }
